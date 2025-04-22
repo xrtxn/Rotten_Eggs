a54269b1d7e211d7cbb791fa0ebe73a7cce2eb77
xrtxn <mihok.martin@protonmail.com>
2025-04-22 14:40:31 +0200

    added generated project

---
+++ src/main/java/com/xrt/tarsas/ertekelo/ErtekeloApplication.java
@@ -0,0 +1,13 @@
+package com.xrt.tarsas.ertekelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErtekeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErtekeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xrt/tarsas/ertekelo/ErtekeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xrt.tarsas.ertekelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErtekeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
